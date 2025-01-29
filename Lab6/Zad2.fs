a14b2eba16ebf3e09a95905952bab0df118a4242
IceScarecrow <115565468+MaksymilianLechowicz@users.noreply.github.com>
2025-01-29 09:47:35 +0100

    Add files via upload

---
+++ Lab6/Zad2.fs
@@ -0,0 +1,16 @@
+﻿module Zad2
+
+open System
+open System.IO
+
+let sprPalindrom(str:string) =
+    let clearText = str.Replace(" ", "").ToLower()
+    clearText = string(Array.rev(clearText.ToCharArray()))
+
+printfn "Podaj text: "
+let inputText = Console.ReadLine()
+
+if sprPalindrom inputText then
+    printfn "Podany text jest palindromem"
+else
+    printfn "Podany text nie jest palindromem"
