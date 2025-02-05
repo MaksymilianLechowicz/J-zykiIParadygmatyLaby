e5f431e6ce8ddeb28a16dc15b0ea07d9287ab010
IceScarecrow <115565468+MaksymilianLechowicz@users.noreply.github.com>
2025-02-05 08:44:54 +0100

    Add files via upload

---
+++ Lab8/Zad2.fs
@@ -0,0 +1,47 @@
+﻿open System
+open System.Collections.Generic
+
+type LinkedList<'T> =
+    | Empty
+    | Node of 'T * LinkedList<'T>
+
+//let Head = 
+//    function
+//    | Empty -> failwith "Lista pusta."
+//    | Node(Head,_) ->Head
+
+//let Tail =
+//    function
+//    | Empty -> failwith "Lista pusta"
+//    | Node(Tail,_) -> Tail
+
+//let addHead value list =
+//    Node(value,list)
+
+//let rec pristList list =
+//    match list with
+//    | Empty -> ()
+//    | Node (value, next) ->
+//        printf "%A" value
+//        pristList next
+
+//[<EntryPoint>]
+//let main argv =
+//    let list1 = Empty
+
+//    pristList list1
+//    let list2 = addHead 1 list1
+//    let list2 = addHead 2 list2
+
+//    0
+
+
+let rec fromList = 
+    function
+        | [] -> Empty
+        | x::xs ->Node(x,fromList xs)
+
+let rec sumList =
+    function
+    | Empty -> 0
+    | Node(H,T) -> H + sumList T
